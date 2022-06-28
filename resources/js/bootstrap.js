0dd36ff027f962acfc2935574190a5eedacf9352
tungdq <dinhquangtung6954@gmail.com>
2022-06-28 22:19:42 +0700

    feat: add npm package

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,11 @@
-import _ from 'lodash';
-window._ = _;
+window._ = require('lodash');
+
+try {
+    window.$ = window.jQuery = require('jquery');
+    window.Popper = require('@popperjs/core');
+    window.bootstrap = require('bootstrap');
+    require('admin-lte')
+} catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -7,8 +13,7 @@ window._ = _;
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
+window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -20,8 +25,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // import Echo from 'laravel-echo';
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+// window.Pusher = require('pusher-js');
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
